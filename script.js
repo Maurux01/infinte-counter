125f67dae36e4f8c08d1c675041c63749e21f12e
Mauro Infante <108948735+Maurux01@users.noreply.github.com>
2025-01-28 11:43:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let counter = 0;
 let intervalId = null;
-const intervalTime = 200; // Intervalo en milisegundos (0.5 segundos)
+const intervalTime = 100; // Intervalo en milisegundos (0.5 segundos)
 
 function updateCounter() {
   document.getElementById('counter').textContent = counter;
